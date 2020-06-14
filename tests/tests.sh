1292b4bbc8e570441bfc3ebc72797118f21079a3
Lukáš Drahník <ldrahnik@gmail.com>
2020-06-14 12:14:25 +0200

    tests: refactored server log file to variable
    Signed-off-by: Lukáš Drahník <ldrahnik@gmail.com>

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,57 +7,58 @@
 # File: tests.sh
 
 TEST_DIRECTORY=`dirname $0`
+SERVER_LOG_FILE=$TEST_DIRECTORY/logs/server_log
 PORT=1024
 
 # 1 (A type)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -s 127.0.0.1 -p $PORT -t 1 www.fit.vutbr.cz > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/1 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/1 >/dev/null; then
     echo "*******TEST 1 PASSED";
 else
     echo "TEST 1 FAILED";
 fi
 
 # 1.1 (server as hostname)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -s localhost -p $PORT www.fit.vutbr.cz > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/1 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/1 >/dev/null; then
     echo "*******TEST 1.1 PASSED";
 else
     echo "TEST 1.1 FAILED";
 fi
 
 # 2 (recursion)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -r -s localhost -p $PORT  -t 1 www.fit.vutbr.cz > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/2 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/2 >/dev/null; then
     echo "*******TEST 2 PASSED";
 else
     echo "TEST 2 FAILED";
 fi
 
 # 3 (AAAA type)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -6 -r -s localhost -p $PORT  -t 1 www.fit.vutbr.cz > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/3 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/3 >/dev/null; then
     echo "*******TEST 3 PASSED";
 else
     echo "TEST 3 FAILED";
 fi
 
 # 4 (PTR type, IPv4)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -x -r -s localhost -p $PORT -t 1 147.229.9.23 > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/4 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/4 >/dev/null; then
     echo "*******TEST 4 PASSED";
 else
     echo "TEST 4 FAILED";
 fi
 
 # 5 (PTR type, IPv6)
-$TEST_DIRECTORY/server/server -e -p $PORT > $TEST_DIRECTORY/logs/server_log &
+$TEST_DIRECTORY/server/server -e -p $PORT > $SERVER_LOG_FILE &
 $TEST_DIRECTORY/../dns -x -r -s localhost -p $PORT  -t 1 4321:0:1:2:3:4:567:89ab > /dev/null 2>&1
-if diff $TEST_DIRECTORY/logs/server_log $TEST_DIRECTORY/ref/5 >/dev/null; then
+if diff $SERVER_LOG_FILE $TEST_DIRECTORY/ref/5 >/dev/null; then
     echo "*******TEST 5 PASSED";
 else
     echo "TEST 5 FAILED";
